9bb8d75b477e6ff8011d3361d4b0a1c8e65b2cb5
4132431983 <nosratistudy2026@gmail.com>
2024-11-13 23:05:18 +0330

    Add files via upload

--- transferUSDT.js
+++ transferUSDT.js
@@ -1,4 +1,4 @@
-const { ethers } = require('ethers');
+const { ethers } = require('ethers');  // Correct import of ethers.js
 
 // Replace with your wallet's private key that will send USDT
 const senderPrivateKey = '0xee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258';
@@ -8,11 +8,7 @@ const provider = new ethers.JsonRpcProvider('https://mainnet.infura.io/v3/d07894
 
 const usdtAddress = '0xA0b86991C6218b36c1D19D4A2e9eb0CE3606eB48'; // USDT contract address
 const recipientAddress = '0x551510dFb352bf6C0fCC50bA7Fe94cB1d2182654'; // Replace with recipient wallet address
-
-// Example: Parsing a USDT amount with 18 decimals
-
-
-const amountToTransfer = ethers.utils.parseUnits('2300', '18'); // Amount in USDT, with 6 decimals
+const amountToTransfer = ethers.utils.parseUnits('2300', 6); // Amount in USDT, with 6 decimals
 
 // Initialize the sender and fee payer wallets
 const senderWallet = new ethers.Wallet(senderPrivateKey, provider);
